3759d3e117bfe097b40c73d17085b404105f5316
HarryPuttar5832 <niranjangnair3@gmail.com>
2022-06-15 13:35:10 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+ "verification": "",
+ "description": "Bluh",
+ "domain": "https://radcliff.cf", // domain
+ "google_analitics": "", // google analitics
+ "token": process.env.TOKEN,
+ "https":"https://", // leave as is
+ "port":"5003"
+}
+
+
+// for dashboard, read more on https://github.com/IgorKowalczyk/majobot
